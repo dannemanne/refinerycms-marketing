fd37e9f0e9f8d4ca650316cc4b1dce32168c0b45
Daniel Wiklund <daniel@happyrabbit.com>
2014-10-23 14:46:07 +0800

    Added specs for contact view

--- spec/support/factories/refinery/contact.rb
+++ spec/support/factories/refinery/contact.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     sequence(:base_id) { |n| n }
     name 'John Doe'
     sequence(:email) { |n| "#{n}@foo.com" }
+    sequence(:tags_joined_by_comma) { |n| 'abcdefghijklmnopqrstuvwxyz'[n] * 5 }
   end
 end
 
