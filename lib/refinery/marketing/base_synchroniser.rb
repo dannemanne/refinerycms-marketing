5fc619481e8312c45e6d459a1a18c31bc2efd257
Daniel Wiklund <daniel@happyrabbit.com>
2015-01-21 16:18:41 +0800

    Fixed problem in base synchroniser

--- lib/refinery/marketing/base_synchroniser.rb
+++ lib/refinery/marketing/base_synchroniser.rb
@@ -11,6 +11,7 @@ module Refinery
       attr_accessor :error
 
       def initialize
+        @all_base_ids = []
         # The API does not support querring at the moment
         #@last_modified_date = Refinery::Contacts::Contact.maximum(:base_modified_at)
       end
@@ -20,8 +21,8 @@ module Refinery
           Refinery::Marketing::Contact.transaction do
             # Loops through all contacts from Base and updates the ones already
             # present in the database and creates the ones that are not
-            base_contacts.each do |base_contact|
-              if (contact = grouped_refinery_contacts[base_contact.id]).present?
+            each_base_contact do |base_contact|
+              if (contact = Refinery::Marketing::Contact.find_by_base_id(base_contact.id)).present?
                 sync_contact contact, base_contact
               else
                 contact = Refinery::Marketing::Contact.new
@@ -30,13 +31,9 @@ module Refinery
               end
             end
 
-            # Goes through all the contacts in the database that were not present
-            # in the list of contacts from Base and flags them as removed.
-            bc_ids = base_contacts.map(&:base_id)
-            refinery_contacts.select { |rc| !bc_ids.include?(rc.id) }.each do |removed_contact|
-              removed_contact.removed_from_base = true
-              removed_contact.save!
-            end
+            # Flags all the contacts that were not found in the latest sync from, as removed.
+            Refinery::Marketing::Contact.where('base_id IS NOT NULL AND base_id NOT IN (?)', @all_base_ids + [-1]).update_all(removed_from_base: true)
+
           end
           true
         rescue StandardError => e
@@ -50,38 +47,41 @@ module Refinery
         @session ||= BaseCrm::Session.new(BASE_TOKEN)
       end
 
-      def base_contacts
-        return @_bc unless @_bc.nil?
-
-        @_bc = []
+      # Loads all contacts from base in batches and yields them one at a time. This
+      # way we avoid to load all contacts into memory at the same time. And we only
+      # store the retrieved ids so that we can look for any existing contacts that
+      # was not found in base, later on.
+      def each_base_contact(&block)
+        @all_base_ids = []
 
         page = 1 # Contacts uses 1 as first page number
         while (contacts = session.contacts.params(page: page).all).any?
-          @_bc += contacts
+          contacts.each do |contact|
+            @all_base_ids << contact.id
+            block.call contact
+          end
           page += 1
         end
 
-        @_bc
+        # Everything okay
+        true
       end
 
-      # Retrieves all Contacts from database that are not flagged as removed
-      # from Base and arranges them in a Hash where the keys are the id's from Base.
-      def refinery_contacts
-        @_rc ||= Refinery::Marketing::Contact.where(removed_from_base: false)
-      end
-
-      def grouped_refinery_contacts
-        @_grc ||= refinery_contacts.inject({}) { |acc, rc| acc.merge(rc.base_id => rc) }
-      end
-
+      # Copies all information from the base contact into the refinery contact.
       def sync_contact(contact, base_contact)
         MAPPED_ATTR.each do |attr|
-          contact.send("#{attr}=", base_contact.send(attr))
+          # Use [attr] method instead of send(attr) because zip attribute is calling Enumerable zip method
+          contact.send("#{attr}=", base_contact[attr])
         end
         if (o = base_contact.organisation).present? && (c = o['contact']).present? && (c_id = c['id']).present?
           contact.organisation_id = c_id
         end
+
+        # Set the update date in case we want to only sync selectively later on
         contact.base_modified_at = base_contact.updated_at
+
+        # Make sure that the contact is not flagged as removed from base
+        contact.removed_from_base = false
         contact.save!
       end
 
